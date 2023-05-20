1f0f5317e7dd4dcddc423d63abce5013d18eb1ed
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-05-20 17:49:04 +0530

    gets  all chats message

--- controllers/chats.js
+++ controllers/chats.js
@@ -1,11 +1,28 @@
 const Messages = require('../models/messages');
+const User = require('../models/user');
 exports.postChatMessages = async (req, res, next) => {
     try {
         const { messages } = req.body;
         if (!messages) {
             return res.status(500).json({ message: 'Sending empty messages are not allowed' });
         }
-        let data = await req.user.createMessage({ messages: messages });
+        await req.user.createMessage({ messages: messages });
+        return res.status(201).json({ messages: 'Successfully updated the message in database' });
+    } catch (error) {
+        res.status(403).json({ message: 'Something went wrong' });
+    }
+}
+exports.getChatMessages = async (req, res, next) => {
+    try {
+        let data = await Messages.findAll({
+            attributes: ['messages'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
         return res.status(201).json(data);
     } catch (error) {
         res.status(403).json({ message: 'Something went wrong' });
--- routes/chats.js
+++ routes/chats.js
@@ -3,4 +3,5 @@ const authenticateRoutes = require('../middlewares/auth');
 const ChatsControllers = require('../controllers/chats');
 const router = express.Router();
 router.post('/saveMessage', authenticateRoutes.authenticate, ChatsControllers.postChatMessages);
+router.get('/getMessage', authenticateRoutes.authenticate, ChatsControllers.getChatMessages);
 module.exports = router;
