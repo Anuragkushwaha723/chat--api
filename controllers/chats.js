7f8b52d623ee6e44c2a32bd1abdce9423d2aab7c
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-05-22 12:22:17 +0530

    update of offset value in getchatmessages api

--- controllers/chats.js
+++ controllers/chats.js
@@ -14,7 +14,22 @@ exports.postChatMessages = async (req, res, next) => {
 }
 exports.getChatMessages = async (req, res, next) => {
     try {
+        let lstUsrId, lstChtId;
+
+        if (req.query.lstUsrId == "undefined") {
+            lstUsrId = 1;
+        } else {
+            lstUsrId = + req.query.lstUsrId + 1;
+        }
+
+        if (req.query.lstChtId == "undefined") {
+            lstChtId = 1;
+        } else {
+            lstChtId = + req.query.lstChtId + 1;
+        }
+
         let promise1 = Messages.findAll({
+            offset: lstChtId - 1,
             attributes: ['messages', 'id'],
             include: [
                 {
@@ -23,7 +38,7 @@ exports.getChatMessages = async (req, res, next) => {
                 }
             ]
         });
-        let promise2 = User.findAll({ attributes: ['name'] });
+        let promise2 = User.findAll({ offset: lstUsrId - 1, attributes: ['name', 'id'] });
         let [data, users] = await Promise.all([promise1, promise2]);
         return res.status(201).json({ messages: data, users: users });
     } catch (error) {
